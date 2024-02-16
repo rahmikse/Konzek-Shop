b5c385988516f838671871b27d1732cd0a425373
rahmikse <rahmikse19@gmail.com>
2024-02-16 12:31:36 +0300

    fixed-top

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import LanguageSwitcher from "./LanguageSwitcher";
+import { useTranslation } from "react-i18next";
 export default function Navbar() {
+  const [t] = useTranslation("global");
+  const state = useSelector((state) => state.cart.cartItems);
+
+  const isActive = (path) => {
+    const currentPath = window.location.pathname;
+    return currentPath === path || currentPath.startsWith(path + "/");
+  };
+
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-white py-3 navbar-light shadow-sm">
+      <nav className="navbar navbar-expand-lg bg-white py-3 navbar-light shadow-sm fixed-top">
         <div className="container">
-          <NavLink className="navbar-brand fw-4 fs-4" to="#">
-            KONZEK SHOP
+          <NavLink className="navbar-brand fw-4 fs-4" to="/">
+            KONZEK
           </NavLink>
           <button
             className="navbar-toggler"
@@ -20,53 +31,63 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0 ">
               <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="/">
-                  Home
+                <NavLink
+                  className={`nav-link ${isActive("/") ? "active" : ""}`}
+                  to="/home"
+                  activeClassName="active"
+                >
+                  {t("navbar.home")}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink
-                  className="nav-link active"
-                  aria-current="page"
+                  className={`nav-link ${
+                    isActive("/products") ? "active" : ""
+                  }`}
                   to="/products"
+                  activeClassName="active"
                 >
-                  Products
+                  {t("navbar.products")}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink
-                  className="nav-link active"
-                  aria-current="page"
+                  className={`nav-link ${isActive("/about") ? "active" : ""}`}
                   to="/about"
+                  activeClassName="active"
                 >
-                  About
+                  {t("navbar.about")}
                 </NavLink>
               </li>
               <li className="nav-item">
                 <NavLink
-                  className="nav-link active"
-                  aria-current="page"
+                  className={`nav-link ${isActive("/contact") ? "active" : ""}`}
                   to="/contact"
+                  activeClassName="active"
                 >
-                  Contact
+                  {t("navbar.contacts")}
                 </NavLink>
               </li>
             </ul>
             <div className="buttons">
               <NavLink to="/login" className="btn btn-outline-dark">
-                <i className="fa fa-sign-in me-1"></i>Login
+                <i className="fa fa-sign-in me-1"></i>
+                {t("register.login")}
               </NavLink>
               <NavLink to="/register" className="btn btn-outline-dark ms-2">
-                <i className="fa fa-user-plus me-1"></i>Register
+                <i className="fa fa-user-plus me-1"></i>
+                {t("register.register")}
               </NavLink>
               <NavLink to="/cart" className="btn btn-outline-dark ms-2">
-                <i className="fa fa-shopping-cart me-1"></i>Cart
+                <i className="fa fa-shopping-cart me-1"></i>
+                {t("register.cart")} ({state.length})
               </NavLink>
             </div>
           </div>
         </div>
+        <LanguageSwitcher />
       </nav>
     </div>
   );
